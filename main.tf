648fa8a295529f5423c2d7d48d376e4839b9d238
AKSHAY2126 <akshaysingh5000@gmail.com>
2024-09-22 10:02:45 +0530

    aks deployment

---
+++ main.tf
@@ -0,0 +1,48 @@
+# Create Resource Group
+resource "azurerm_resource_group" "aks_rg" {
+  name     = "aks-resource-group"
+  location = "East US"
+}
+
+
+# Create Azure Container Registry
+resource "azurerm_container_registry" "acr" {
+  name                = "mycontainerregistry"
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  location            = azurerm_resource_group.aks_rg.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+
+# Create AKS Cluster
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "aks-cluster"
+  location            = azurerm_resource_group.aks_rg.location
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  dns_prefix          = "aksdns"
+  kubernetes_version  = "1.26.3" # Use the desired Kubernetes version
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_DS2_v2"
+    os_disk_size_gb = 30
+    type       = "VirtualMachineScaleSets"  # For scaling flexibility
+    max_pods   = 30
+  }
+
+  network_profile {
+    network_plugin = "azure" # Use Azure CNI
+    load_balancer_sku = "Standard"
+    outbound_type     = "loadBalancer"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+
+  tags = {
+    Environment = "Dev"
+  }
+}
